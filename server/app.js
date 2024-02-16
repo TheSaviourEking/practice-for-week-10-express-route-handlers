7d4b33b10ac210cb580a629eab4f7f56618f9e86
TheSaviourEking <saviour.eking@outlook.com>
2024-02-16 01:55:31 +0100

    chore: complete practice project

--- server/app.js
+++ server/app.js
@@ -55,6 +55,83 @@ app.get('/artists/latest/albums', (req, res) => {
     .send(getAlbumsForLatestArtist());
 })
 
+app.get('/artists/:artistId', (req, res) => {
+  res.status(200)
+    .set('Content-Type', 'application/json')
+    .send(getArtistByArtistId(req.params.artistId));
+})
+
+app.put('/artists/:artistId', editArtistHandler);
+app.patch('/artists/:artistId', editArtistHandler);
+
+function editArtistHandler(req, res) {
+  res.status(200)
+    .set('Content-Type', 'application/json')
+    .send(editArtistByArtistId(req.params.artistId, req.body));
+}
+
+app.delete('/artists/:artistId', (req, res) => {
+  const artistId = req.params.artistId;
+  if (getArtistByArtistId(artistId)) {
+    deleteArtistByArtistId(artistId);
+
+    res.status(200)
+      .set('Content-Type', 'application/json')
+      .send({ message: "Successfully deleted" })
+  }
+
+})
+
+app.get('/artists/:artistId/albums', (req, res) => {
+  res.status(200)
+    .set('Content-Type', 'application/json')
+    .send(getAlbumsByArtistId(req.params.artistId));
+})
+
+app.get('/albums/:albumId', (req, res) => {
+  res.status(200)
+    .set('Content-Type', 'application/json')
+    .send(getAlbumByAlbumId(req.params.albumId));
+})
+
+app.post('/artists/:artistId/albums', (req, res) => {
+  res.status(201)
+    .set('Content-Type', 'application/json')
+    .send(addAlbumByArtistId(req.params.artistId, req.body));
+})
+
+app.put('/albums/:albumId', editAlbumHandler)
+app.patch('/albums/:albumId', editAlbumHandler);
+
+function editAlbumHandler(req, res) {
+  res.status(200)
+    .set('Content-Type', 'application/json')
+    .send(editAlbumByAlbumId(req.params.albumId, req.body));
+}
+
+app.delete('/albums/:albumId', (req, res) => {
+  const albumId = req.params.albumId;
+  if (getAlbumByAlbumId(albumId)) {
+    deleteAlbumByAlbumId(albumId);
+
+    res.status(200)
+      .set('Content-Type', 'application/json')
+      .send({ message: "Successfully deleted" })
+  }
+})
+
+app.get('/albums', (req, res) => {
+  res.status(200)
+    .set('Content-Type', 'application/json')
+    .send(getFilteredAlbums(req.query.startsWith));
+})
+
+app.get('/songs/:songId', (req, res) => {
+  res.status(200)
+    .set('Content-Type', 'application/json')
+    .send(getSongBySongId(req.params.songId));
+})
+
 // DO NOT MODIFY
 if (require.main === module) {
   const port = 8000;
